cd1e104f09fd0cbe0b4f8ff2aae37882244bfd92
Kim Hsiao <white.shopping@gmail.com>
2018-03-19 00:19:05 +0800

    - Add phpmyadmin

--- laravel-start.sh
+++ laravel-start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker-compose up -d php-fpm nginx postgres-postgis mongo redis rabbitmq memcached elasticsearch php-worker
+docker-compose up -d php-fpm nginx mariadb mongo redis rabbitmq memcached elasticsearch php-worker phpmyadmin
