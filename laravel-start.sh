efe1df5921e0a3fafc0b0c440181351d26d83f28
Kim Hsiao <white.shopping@gmail.com>
2018-11-19 23:45:25 +0800

    - Fix mysql version setting error - Remove phpmyadmin default username / password - Update phpmyadmin default port - Set laravel-start.sh, set mysql as default database

--- laravel-start.sh
+++ laravel-start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker-compose up -d php-fpm nginx mariadb mongo redis rabbitmq memcached elasticsearch php-worker phpmyadmin kibana logstash
+docker-compose up -d php-fpm nginx mysql mongo redis rabbitmq memcached elasticsearch php-worker phpmyadmin kibana logstash
